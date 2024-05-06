14e772679b55e46f7fca23bc7d9a68aa4ad62cad
Ефим <fimakrut@mail.ru>
2024-05-07 03:08:23 +0500

    Разработанные модели данных. Ярыгин

---
+++ ShopContent_Yar/Modell/Categorys.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent_Yar.Modell
+{
+    public class Categorys : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
